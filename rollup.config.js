ef266462657ef6b9e1a28a0583c8b63356a0ca07
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-05 17:10:38 +0900

    Configure webpack

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
-import resolve from "rollup-plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import json from "rollup-plugin-json";
-import { terser } from "rollup-plugin-terser";
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import { terser } from 'rollup-plugin-terser';
 import path from 'path';
 import fs from 'fs';
 
 export default {
-    input: "./svgo/src/index.ts",
+    input: './svgo/src/index.ts',
     output: {
-        file: "./svgo/index.js",
-        format: "esm"
+        file: './svgo/index.js',
+        format: 'esm',
     },
     plugins: [
         resolve({
-            preferBuiltins: true 
+            preferBuiltins: true,
         }),
         typescript(),
         commonjs(),
@@ -22,12 +22,14 @@ export default {
         terser(),
         {
             resolveId: id => {
-                const shimsPath = path.resolve(__dirname, "./svgo/shims");
-                const shimNames = fs.readdirSync(shimsPath).map(file => file.split(".")[0]);
+                const shimsPath = path.resolve(__dirname, './svgo/shims');
+                const shimNames = fs
+                    .readdirSync(shimsPath)
+                    .map(file => file.split('.')[0]);
                 if (shimNames.includes(id)) {
-                    return path.join(shimsPath, id + ".ts");
+                    return path.join(shimsPath, id + '.ts');
                 }
-            }
-        }
-    ]
-}
+            },
+        },
+    ],
+};
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Heading = () => {
+    return <h1>foo</h1>;
+};
+
+ReactDOM.render(<Heading />, document.querySelector('.root'));
